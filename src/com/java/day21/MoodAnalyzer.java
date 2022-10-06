cae7de700d9bbd49f65463fb7982ceb46f037ce8
rizry <rizzwan.ansarii@gmail.com>
2022-10-06 23:10:15 +0530

    added test cases for message

--- src/com/java/day21/MoodAnalyzer.java
+++ src/com/java/day21/MoodAnalyzer.java
@@ -2,4 +2,8 @@ package com.java.day21;
 
 public class MoodAnalyzer {
 
+  public String analyzeMood(String message) {
+    if (message.toLowerCase().contains("sad")) return "SAD";
+      else return "HAPPY";
+  }
 }
---
+++ test/com/java/day21/TestMoodAnalyzer.java
@@ -0,0 +1,22 @@
+package com.java.day21;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestMoodAnalyzer {
+
+		@Test
+		public void testMessageContainsSad_thenHappy() {
+				MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+				String mood = moodAnalyzer.analyzeMood("This is a sad message");
+				assertEquals("SAD", mood);
+		}
+
+		@Test
+		public void testMessageContainsHappy_thenHappy() {
+				MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+				String mood = moodAnalyzer.analyzeMood("This is a happy message");
+				assertEquals("HAPPY", mood);
+		}
+
+}
